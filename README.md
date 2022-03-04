9c59ae71ed2115b6f2a16160e31f68c7a6ceea52
perchik_air <1919ars@gmail.com>
2022-03-04 11:07:51 +0200

    (air)

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 - [paper](https://arxiv.org/pdf/2110.15349.pdf)
 - [github | gym OpenAI](https://github.com/Arseni1919/Learning_Gym_OpenAI#the-environment)
 - [gym OpenAI](https://gym.openai.com/envs/#classic_control)
+- [github | Minimalistic Gridworld Environment (MiniGrid)](https://github.com/maximecb/gym-minigrid)
+- [github | marlgrid](https://github.com/kandouss/marlgrid)
--- env_final_goal.py
+++ env_final_goal.py
@@ -1,16 +1,43 @@
+import gym
+import numpy as np
+
 from GLOBALS import *
 
 
 class FinalGoal(gym.Env):
     def __init__(self):
         self.side = 15
-
-    def step(self, action):
-        pass
+        self.field = np.ndarray((self.side, self.side))
+        self.n_goal_tiles = 1
+        self.n_obstacle_tiles = 25
+        self.n_agents = 3
+        self.obs_side = 7
+        self.n_actions = 5
+        self.max_episode = 512
+        self.action_spaces = {}
+        self.observation_spaces = {}
 
     def reset(self):
+        # create action and observation spaces
+        for i_agent in range(self.n_agents):
+            name = f'agent_{i_agent}'
+            self.action_spaces[name] = gym.spaces.Discrete(self.n_actions)
+            self.observation_spaces[name] = np.ndarray((self.obs_side, self.obs_side))
+
+        # create field
+        # -1 - wall, 0 - neutral, 1 - robot, 2 - target
+
+        # update observations
+
+        # return observations
+
+    def step(self, action):
         pass
 
     def render(self, mode="human"):
         pass
 
+
+game = FinalGoal()
+game.reset()
+print(game)
